256d3fcd6390b70c1f6f177d560ddff81d0db701
Eric Davis <edavis@littlestreamsoftware.com>
2009-04-08 02:55:11 +0800

    [#2359] Added Status#hash_hashtag? to check if a message has a hashtag.
    Signed-off-by: Brian Terlson <btthalion@gmail.com>

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,10 @@
 class Status < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+
+  Hashtag = /(#\S+)/
+  
+  def has_hashtag?
+    return (message && message.match(Hashtag)) ? true : false
+  end
 end
---
+++ spec/models/status_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Status, "#has_hashtag?" do
+  it 'should be false when there is no message' do
+    Status.new.has_hashtag?.should be_false
+  end
+  
+  it 'should be false when 0 hashtags are present' do
+    Status.new(:message => 'Test status').has_hashtag?.should be_false
+  end
+
+  it 'should be true when 1 hashtag is present' do
+    Status.new(:message => 'Test with hashtag #tagged').
+      has_hashtag?.should be_true
+  end
+
+  it 'should be true when more than 1 hashtag is present' do
+    Status.new(:message => 'Test with hashtag #tagged #business #redmine #plugin').
+      has_hashtag?.should be_true
+  end
+end
