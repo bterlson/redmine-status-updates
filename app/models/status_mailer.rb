40de4fc20baed8bd35c43caf37024f1a928049fb
Brian Terlson <brian.terlson@gmail.com>
2009-08-08 15:15:30 -0500

    Manually specifying mailer template parts in order to work around Engines and Rails 2.2.2 problem.

--- app/models/status_mailer.rb
+++ app/models/status_mailer.rb
@@ -2,13 +2,31 @@ class StatusMailer < Mailer
   def realtime_notification(status)
     recipients status.recipients
     subject "Status Update"
-    body :author => status.user, :message => status.message
+    content_type    "multipart/alternative"
+
+    part :content_type => "text/html",
+      :body => render_message("realtime_notification.text.html.erb", :author => status.user, :message => status.message)
+
+    part "text/plain" do |p|
+      p.body = render_message("realtime_notification.text.plain.erb", :author => status.user, :message => status.message)
+      p.transfer_encoding = "base64"
+    end
+
   end
 
   def delayed_notification(user, statuses)
     recipients [user.mail]
     subject "Status Update Digest"
+    content_type    "multipart/alternative"
+
     last_update = user.status_notification.last_updated_at
-    body :statuses => statuses, :last_updated_at => last_update
+
+    part :content_type => "text/html",
+      :body => render_message("delayed_notification.text.html.erb", :statuses => statuses, :last_updated_at => last_update)
+
+    part "text/plain" do |p|
+      p.body = render_message("delayed_notification.text.plain.erb", :statuses => statuses, :last_updated_at => last_update)
+      p.transfer_encoding = "base64"
+    end
   end
 end
