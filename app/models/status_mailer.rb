794d01d86380a65fe93a2a76d2f632713f2c3702
bterlson <btthalion@gmail.com>
2009-08-20 11:49:21 -0700

    Specify proper template names given earlier revert.

--- app/models/status_mailer.rb
+++ app/models/status_mailer.rb
@@ -5,10 +5,10 @@ class StatusMailer < Mailer
     content_type    "multipart/alternative"
 
     part :content_type => "text/html",
-      :body => render_message("realtime_notification.text.html.erb", :author => status.user, :message => status.message)
+      :body => render_message("realtime_notification.text.html.rhtml", :author => status.user, :message => status.message)
 
     part "text/plain" do |p|
-      p.body = render_message("realtime_notification.text.plain.erb", :author => status.user, :message => status.message)
+      p.body = render_message("realtime_notification.text.plain.rhtml", :author => status.user, :message => status.message)
       p.transfer_encoding = "base64"
     end
 
@@ -22,10 +22,10 @@ class StatusMailer < Mailer
     last_update = user.status_notification.last_updated_at
 
     part :content_type => "text/html",
-      :body => render_message("delayed_notification.text.html.erb", :statuses => statuses, :last_updated_at => last_update)
+      :body => render_message("delayed_notification.text.html.rhtml", :statuses => statuses, :last_updated_at => last_update)
 
     part "text/plain" do |p|
-      p.body = render_message("delayed_notification.text.plain.erb", :statuses => statuses, :last_updated_at => last_update)
+      p.body = render_message("delayed_notification.text.plain.rhtml", :statuses => statuses, :last_updated_at => last_update)
       p.transfer_encoding = "base64"
     end
   end
